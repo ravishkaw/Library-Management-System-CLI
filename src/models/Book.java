d116366f7dc69aab5d306555d68747a62d0cd1a6
Ravishka <127718357+ravishkaw@users.noreply.github.com>
2024-08-21 21:02:46 +0530

    add Book model

---
+++ src/models/Book.java
@@ -0,0 +1,54 @@
+package models;
+
+public class Book {
+
+    private String id;
+    private String title;
+    private String author;
+    private String genre;
+    private boolean isBorrowed;
+
+    public Book(String bookId, String bookName, String author, String genre) {
+        this.id = bookId;
+        this.title = bookName;
+        this.author = author;
+        this.genre = genre;
+    }
+
+    public String getBookId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public boolean isBorrowed() {
+        return isBorrowed;
+    }
+
+    public void setBorrowed(boolean borrowed) {
+        this.isBorrowed = borrowed;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id='" + id + '\'' +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", genre='" + genre + '\'' +
+                ", isBorrowed=" + isBorrowed +
+                '}';
+    }
+
+}
